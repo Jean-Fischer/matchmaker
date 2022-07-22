9f60f1f697279766f769a63c2137ac7c3854f2e7
Jean Fischer <jeanfischer0@gmail.com>
2022-07-22 23:22:05 +0200

    added: first functionnal version of graphql endpoint

---
+++ WebApi/GraphQL/Query.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Business.Dto;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.GraphQL;
+
+public class Query
+{
+    public IQueryable<MatchDto> Dto(MatchMakingContext context, [Service] IMapper mapper) =>
+        context.Matches
+            .Include(s=>s.Participations)
+            .ThenInclude(s=>s.Player)
+            .AsSplitQuery().ProjectTo<MatchDto>(mapper.ConfigurationProvider);
+
+}
